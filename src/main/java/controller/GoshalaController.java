c74d1d5164c69abb904287c256f869901b6d16fc
Fusion <chetanlashinker9@gmail.com>
2017-07-05 16:09:06 +0530

    last

--- src/main/java/controller/GoshalaController.java
+++ src/main/java/controller/GoshalaController.java
@@ -18,12 +18,13 @@ import model.Members;
 @Controller
 public class GoshalaController{
 	
+	final String uri = "https://goshalawebservice.herokuapp.com";
+	
 	@RequestMapping(value="addGalleryData", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String addGalleryData(@Valid Gallery gallery, Model model) {
-		final String uri = "http://localhost:8080/addGallery";
 	    Gallery gal = new Gallery(gallery.getUrl());
 	    RestTemplate restTemplate = new RestTemplate();
-	    Response obj = restTemplate.postForObject(uri, gal, Response.class);
+	    Response obj = restTemplate.postForObject(uri + "/addGallery", gal, Response.class);
 	    model.addAttribute("response", obj);
 	    return "result";
 	}
@@ -31,20 +32,18 @@ public class GoshalaController{
 	
 	@RequestMapping(value="addMemberData", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String addMemberData(@Valid Members member, Model model) {
-		final String uri = "http://localhost:8080/addMembers";
 	    Members mem = new Members(member.getName(), member.getNumber(), member.getType(), member.getDesignation(), member.getUrl());
 	    RestTemplate restTemplate = new RestTemplate();
-	    Response obj = restTemplate.postForObject(uri, mem, Response.class);
+	    Response obj = restTemplate.postForObject(uri + "/addMembers", mem, Response.class);
 	    model.addAttribute("response", obj);
 	    return "result";
 	}
 	
 	@RequestMapping(value="addGosalaData", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String addGosalaData(@Valid GoshalaData goshalaData, Model model) {
-		final String uri = "http://localhost:8080/addGoshalaData";
 	    GoshalaData goData = new GoshalaData(goshalaData.getPage(), goshalaData.getText(), goshalaData.getUrl());
 	    RestTemplate restTemplate = new RestTemplate();
-	    Response obj = restTemplate.postForObject(uri, goData, Response.class);
+	    Response obj = restTemplate.postForObject(uri + "/addGoshalaData", goData, Response.class);
 	    model.addAttribute("response", obj);
 	    return "result";
 	}
